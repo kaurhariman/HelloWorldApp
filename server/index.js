be55b0dafdb05ecc22c02149482a4af97281fb7b
attriaman <attriaman2520@seasiainfotech.com>
2024-01-04 02:18:49 +0530

    feat(helloworldapp) : :sparkles: Basic Structure
    Initial classes for angular app and node server added

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const { appendFile } = require('fs');
+const express = require('express')
+const cors = require("cors")
+const http = require('http');
+const port = process.env.PORT || 3000;
+const app = express();
+const path = require('path');
+const distDir = path.join(__dirname, '../dist/helloworldapp')
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node!\n'
+  res.end(msg);
+});
+
+app.use(cors())
+app.use(express.static(distDir))
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
+
+app.get('/*', function(req,res){
+  res.sendFile(path.join(distDir + '/index.html'))
+});
+
+// app.use('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
